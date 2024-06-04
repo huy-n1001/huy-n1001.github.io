611297d1283481bcb89c32c424aed355265b2fc7
Huy Nguyen <58452178+huy-n1001@users.noreply.github.com>
2024-06-04 15:48:13 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+//
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink');
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink');
+        }
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Transparent navbar before scrolling
+    const transparentNavbar = function () {
+        const navbar = document.body.querySelector('#mainNav');
+        if (!navbar) {
+            return;
+        }
+        if (window.scrollY > 50) {
+            navbar.classList.remove('transparent');
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+            navbar.classList.add('transparent');
+        }
+    };
+
+    // Initially set the navbar to be transparent
+    transparentNavbar();
+
+    // Change navbar transparency when page is scrolled
+    document.addEventListener('scroll', transparentNavbar);
+});
